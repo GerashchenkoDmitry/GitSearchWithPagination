2260e6fcc11a798d5092e7f4e1d55ecae47ebfc1
GerashchenkoDmitry <finiqw@gmail.com>
2021-12-01 15:14:09 +0700

    Initial Commit

---
+++ InfiniteListScroll/InfiniteListScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteListScrollApp.swift
+//  InfiniteListScroll
+//
+//  Created by Дмитрий Геращенко on 01.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteListScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
